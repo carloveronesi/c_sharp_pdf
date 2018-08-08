89086b5407d4966eaeee6c6fd43e801b7f190f64
c.veronesi <veronesicarlo@outlook.it>
2018-08-08 16:11:03 +0200

    Pulizia e riodino delle importazioni

--- ElaborazionePdf/Test.cs
+++ ElaborazionePdf/Test.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using IDSign.PdfUtility;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-using static iTextSharp.text.pdf.AcroFields;
 
 namespace ElaborazionePdf
 {
