58628051248fab3b697b1ac0a055001a17d3d281
c.veronesi <veronesicarlo@outlook.it>
2018-08-08 09:54:31 +0200

    Aggiunta della dll

---
+++ IDSign.PdfUtility/PdfUtility.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using static iTextSharp.text.pdf.AcroFields;
+
+namespace IDSign.PdfUtility
+{
+	//Logger delegate protorype
+	public delegate void LoggerFunction(string text);
+
+	public class PdfUtility : IDisposable
+	{
+		private string filename;                        //Filename
+		private string filename_out;                    //Filename for the modified file
+		private PdfStamper stamper = null;
+		private PdfReader reader = null;
+		private MemoryStream memoryStream = null;
+		private bool stamperDisposed = false;           //Indicating if some resources has been disposed				
+		static LoggerFunction delegateFunction = null;  //Logger function delegate
+
+		/*!
+		 Constructor
+		 */
+		public PdfUtility(string filename, LoggerFunction funct)
+		{
+			//Delegating logger
+			delegateFunction = funct;
+			//Saving filename
+			this.filename = filename;
+			//Generating output file name (filename + _modified.pdf)
+			Filename_out = filename.Substring(0, filename.Length - 4) + "_modified.pdf";
+			//Loading file
+			LoadFile();
+		}
+
+		/*!
+		Getters and setters
+		*/
+		public string Filename { get => filename; set => filename = value; }
+		public string Filename_out { get => filename_out; set => filename_out = value; }
+
+		/*!
+		 Implementing Dispose()
+		 */
+		#region IDisposable Members
+		~PdfUtility()
+		{
+			Dispose(false);
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				//Disposing Stamper if not alerady disposed
+				if (!stamperDisposed && stamper != null)
+				{
+					try
+					{
+						stamper.Dispose();
+					}
+					catch (Exception e)
+					{
+						//Calling logger
+						Log("ERROR WHILE DIPOSING STAMPER:\n" + e.ToString());
+					}
+				}
+
+				//Disposing reader
+				if (reader != null)
+				{
+					try
+					{
+						reader.Dispose();
+					}
+					catch (Exception e)
+					{
+						//Calling logger
+						Log("ERROR WHILE DIPOSING READER:\n" + e.ToString());
+					}
+				}
+
+				//Disposing memoryStream
+				if (memoryStream != null)
+				{
+					try
+					{
+						memoryStream.Dispose();
+					}
+					catch (Exception e)
+					{
+						//Calling logger
+						Log("ERROR WHILE DIPOSING MEMORYSTREAM:\n" + e.ToString());
+					}
+				}
+			}
+		}
+		#endregion
+
+		/*!
+		 Loading the input file into memory
+		 */
+		private void LoadFile()
+		{
+			memoryStream = new MemoryStream();
+			reader = new PdfReader(filename);
+			stamper = new PdfStamper(reader, memoryStream)
+			{
+				FormFlattening = true
+			};
+		}
+	}
+}
