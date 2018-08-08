344c88e3dc9ee7127e7367673b733a0fd9981135
c.veronesi <veronesicarlo@outlook.it>
2018-08-08 10:55:35 +0200

    Creazione eccezione custom

---
+++ IDSign.PdfUtility/Exceptions/FieldNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace IDSign.PdfUtility
+{
+	public class FieldNotFoundException : Exception
+	{
+		public FieldNotFoundException()
+		{}
+		public FieldNotFoundException(string message) : base("Field not found: " + message)
+		{}
+		public FieldNotFoundException(string message, Exception innerException)
+		: base("Field not found: " + message, innerException)
+		{}
+		protected FieldNotFoundException(SerializationInfo info, StreamingContext ctxt)
+			: base(info, ctxt)
+		{ }
+	}
+}
